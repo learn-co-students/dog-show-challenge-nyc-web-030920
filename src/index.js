1830e65b795e437f4c12887fe44ffa268369c5f0
Samanthaponce5 <samanthaponce5@gmail.com>
2020-04-29 11:11:51 -0400

    one-hour-in

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 document.addEventListener('DOMContentLoaded', () => {
+    getDog()
 
-})
+});
+//================================================
+function getDog() {
+fetch('http://localhost:3000/dogs')
+	.then((resp) => resp.json())
+	.then((dogs) => {
+        let table = document.getElementById('table-body')
+        dogs.forEach(function(dog){
+        let tr = document.createElement('tr')
+        tr.dataset.id = dog.id
+        tr.innerHTML = `
+			<td>${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td>
+			<td>
+				<button class='editBtn'>Edit</button>
+			</td>
+        `
+        table.appendChild(tr)
+       })
+    editBtn()
+
+    });
+
+ }
+ //============================================================
+function editBtn(){
+    let btn = document.getElementsByTagName('button')
+    document.addEventListener('click', function(e){
+        if(e.target.className == 'editBtn'){
+        //console.log(e.target.parentNode.parentNode.children[2].innerText)
+         let form = document.getElementById('dog-form')
+        // form.addEventListener('submit',function(){
+
+            form.name.value = e.target.parentNode.parentNode.children[0].innerText
+            form.breed.value=e.target.parentNode.parentNode.children[1].innerText
+            form.sex.value = e.target.parentNode.parentNode.children[2].innerText
+        //})
+
+
+
+
+
+        // let id = e.target.parentNode.parentNode.dataset.id
+        // fetch(`http://localhost:3000/dogs/${id}`,{
+        //     method:'PATCH',
+        //     headers:{
+        //         'Content-Type':'application/json',
+        //         'Accept':'application/json'
+        //     },
+        //     body:JSON.stringify({
+
+        //     })
+        // })
+        // .then((resp)=>resp.json())
+        
+        }
+
+    })
+}
