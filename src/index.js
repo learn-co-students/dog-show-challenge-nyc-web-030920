db38b1dad5f0e66c8cf66acb3434004b854f16a0
MisterC1ean <robert.keller22@gmail.com>
2020-04-29 11:44:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("The DOM is loaded!")
+    init()
 
-
+    function init(){
     fetch('http://localhost:3000/dogs')
         .then(response => response.json())
         .then(json => json.forEach(loadDog))
-    
+    }
 
     const dogTable = document.querySelector("table")
     const editForm = document.querySelector("form")
@@ -43,10 +44,30 @@ document.addEventListener('DOMContentLoaded', () => {
         // make ptch request to dog id and update info in API
         // make get request to get new dog table information AFTER PATCH
         e.preventDefault()
-        console.log(e.target)
-        console.log("stopped the submit")
-    })
+        const id = e.target.dataset.id
+        // console.log(e.target.name.value)
+        newDog = {
+            name: e.target.name.value,
+            breed: e.target.breed.value,
+            sex: e.target.sex.value
+        }
+        fetch(`http://localhost:3000/dogs/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'accept': 'application/json',
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newDog)
+            })
+            .then(response => response.json())
+            .then(dog => {
+                let foundDog = document.querySelector(`tr[data-id="${dog.id}"]`)
+                    foundDog.children[0].innerText = dog.name
+                    foundDog.children[1].innerText = dog.breed
+                    foundDog.children[2].innerText = dog.sex
+            })
 
+    })
 
 //!END OF THE DOM LISTENER
 })
