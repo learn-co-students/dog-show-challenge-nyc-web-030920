4b284bb2e3b2c74a244d4fc90df69abf6cbfa5e7
Samanthaponce5 <samanthaponce5@gmail.com>
2020-04-29 11:41:46 -0400

    final

--- src/index.js
+++ src/index.js
@@ -26,36 +26,54 @@ fetch('http://localhost:3000/dogs')
  }
  //============================================================
 function editBtn(){
-    let btn = document.getElementsByTagName('button')
+    //let btn = document.getElementsByTagName('button')
     document.addEventListener('click', function(e){
         if(e.target.className == 'editBtn'){
         //console.log(e.target.parentNode.parentNode.children[2].innerText)
          let form = document.getElementById('dog-form')
         // form.addEventListener('submit',function(){
 
-            form.name.value = e.target.parentNode.parentNode.children[0].innerText
-            form.breed.value=e.target.parentNode.parentNode.children[1].innerText
-            form.sex.value = e.target.parentNode.parentNode.children[2].innerText
+        let name = form.name.value = e.target.parentNode.parentNode.children[0].innerText
+        let breed = form.breed.value=e.target.parentNode.parentNode.children[1].innerText
+        let sex = form.sex.value = e.target.parentNode.parentNode.children[2].innerText
         //})
 
+        let id = e.target.parentNode.parentNode.dataset.id
+        let table = document.getElementById('table-body')
 
 
+        form.addEventListener('submit', function(e){
+            e.preventDefault()
+            let newName = e.target.name.value
+            let newBreed = e.target.breed.value
+            let newSex = e.target.sex.value
+        fetch(`http://localhost:3000/dogs/${id}`,{
+            method:'PATCH',
+            headers:{
+                'Content-Type':'application/json',
+                'Accept':'application/json'
+            },
+            body:JSON.stringify({
+                name:newName,
+                breed:newBreed,
+                sex:newSex
+            })
+        })
+        .then((resp)=>resp.json())
+        .then((dog)=> {
+        //let tr = document.getElementsByTagName('tr')
+        let tr = document.createElement('tr')
+        tr.innerHTML = `
+			<td>${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td>
+			
+        `
+       table.appendChild(tr)
+        })
 
+         })
 
-        // let id = e.target.parentNode.parentNode.dataset.id
-        // fetch(`http://localhost:3000/dogs/${id}`,{
-        //     method:'PATCH',
-        //     headers:{
-        //         'Content-Type':'application/json',
-        //         'Accept':'application/json'
-        //     },
-        //     body:JSON.stringify({
-
-        //     })
-        // })
-        // .then((resp)=>resp.json())
-        
         }
+   
 
     })
 }
