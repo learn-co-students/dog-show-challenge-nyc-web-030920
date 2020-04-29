33a05b0f59a2707b17d24ff0315264e1b10540b5
JohnsonNYC <jkow95@gmail.com>
2020-04-29 11:41:15 -0400

    second

--- src/index.js
+++ src/index.js
@@ -8,27 +8,30 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(array => array.forEach(element =>{
         addToTable(element)
     }))
-    
+    // why can't i define name node here to get my id??
+
     let submitButton = document.querySelector('input[type=submit]')
     submitButton.addEventListener('click', event =>{
         event.preventDefault()
-        // fetch(`http://localhost:3000/dogs/(HOW DO I GET THIS ID)`)
+        // console.log(`http://localhost:3000/dogs/${hash.id}`)
         
     })
+    
 })
 
 function addToTable(hash){
     let tr = document.createElement('tr')
     tr.innerHTML = `<td id=${hash.id}>${hash.name}</td> <td>${hash.breed}</td> <td>${hash.sex}</td> <td><button>Edit</button></td>`
     tableContainer.appendChild(tr)
+    let editButton = tr.querySelector('button') // went to parent to get button for each element 
+    let nameNode = tr.querySelector('td')
 
-    let editButton = tr.querySelector('button') // went to paren to get button for each element 
     editButton.addEventListener('click',event => {
         nameInput = document.querySelector(`input[name=name]`)
         BreedInput = document.querySelector('input[name=breed]')
         sexInput = document.querySelector('input[name=sex]')
-        
-        nameInput.value = hash.name
+
+        nameInput.value = nameNode.innerText // Hit a road block here
         BreedInput.value = hash.breed
         sexInput.value = hash.sex
     })
