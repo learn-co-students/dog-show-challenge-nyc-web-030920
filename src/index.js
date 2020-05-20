f5de594b377c84886bd5891ee49aca892fc60e18
Eliot Henson <eliotrhenson@gmail.com>
2020-05-20 10:24:51 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-
+const dogUrl = 'http://localhost:3000/dogs'
+const dogTable = document.querySelector(".table-body")
+console.log(dogTable)
+document.addEventListener("DOMContentLoaded", e => {
+    fetchDogs()
 })
+
+const fetchDogs = () => {
+    fetch(dogUrl)
+    .then(res => res.json())
+    .then(renderDogs)
+}
+
+const renderDogs = (dogs) => {
+    dogs.forEach(dog => {
+        const dogTr = document.createElement("tr")
+        dogTr.dataset.id = dog.id
+        dogTr.className = "dog"
+        dogTr.innerHTML = `
+        <td>${dog.name}</td> <td>*${dog.breed}*</td> <td>*${dog.sex}*</td> <td><button>Edit</button></td>
+        `
+        // console.log(dogTr)
+        // dogTable.appendchild(dogTr)
+    })
+}
